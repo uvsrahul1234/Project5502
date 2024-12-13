f6da11c069211e83d4b978706659858b371670d3
Rahul <uvsrahul@gmail.com>
2024-12-13 12:32:46 -0600

    test_subscribe commented 2 lines

--- tests/test_predict_subscribe.py
+++ tests/test_predict_subscribe.py
@@ -23,22 +23,3 @@ def test_make_prediction():
     assert isinstance(result['Subscribes'], np.float64)
     # assert math.isclose(result['Misses Out'], misses, abs_tol=0.01)
     # assert math.isclose(result['Subscribes'], subscribes, abs_tol=0.01)
-    
-# def test_make_prediction():
-#     # Given
-#     watch = -8.1
-#     duration = 1.4
-#     ctr = -0.7
-#     interest = 0
-#     misses = 0.32
-#     subscribes = 0.67
-
-#     # When
-#     result = predict_subscribe(watch, duration, ctr, interest)
-
-#     # Then
-#     assert isinstance(result, dist)
-#     assert isinstance(result['Misses Out'], np.float64)
-#     assert isinstance(result['Subscribes'], np.float64)
-#     assert math.isclose(result['Misses Out'], misses, abs_tol=0.01)
-#     assert math.isclose(result['Subscribes'], Subscribes, abs_tol=0.01)
